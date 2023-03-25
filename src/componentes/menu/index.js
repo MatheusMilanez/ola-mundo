bb27b5dc29a887abc8c8301bf79b4d54a7bf2eb8
Matheus Milanez <matheusmmlopes19@gmail.com>
2023-03-25 17:03:42 -0300

    Adicionando o Menu + Banner

---
+++ src/componentes/menu/index.js
@@ -0,0 +1,17 @@
+
+import MenuLink from '../MenuLink';
+import styles from './menu.module.css';
+
+
+export default function Menu(){
+    
+
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <MenuLink to="/">Inicio</MenuLink>
+                <MenuLink to="/sobremim">Sobre Mim</MenuLink>
+            </nav>
+        </header>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./componentes/menu";
 import Inicio from "./Paginas/Inicio";
 import SobreMim from "./Paginas/SobreMim";
 
 
-
-
-function App() {
+function AppRoutes() {
   return (
     <BrowserRouter>
+      <Menu/>
+      
       <Routes>
         <Route path="/" element={<Inicio/>}/>
         <Route path="/sobremim" element={<SobreMim/>}/>
@@ -17,7 +18,7 @@ function App() {
     );
 }
 
-export default App;
+export default AppRoutes;
 
 
 
